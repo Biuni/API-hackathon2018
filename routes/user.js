ff9f56870bc889ce29a02e931cb5cf52f12340e8
Biuni <info.gianlucabonifazi@gmail.com>
2018-11-29 19:58:06 +0100

    Added User login

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const crypto = require('crypto')
+const moment = require('moment')
 const db = require('../utils/db')
 const promisify = require('../utils/promisify')
 
@@ -20,21 +22,37 @@ router.post('/login', (req, res, next) => {
       message: 'Error. Try again!'
     })
   }
+  let user_id
   promisify.query('SELECT `id` FROM `user` WHERE `email` = ? AND `password` = ?', [req.body.username, req.body.password])
     .then(userId => {
-      if (userId[0].userId === undefined) {
+      if (userId[0] == undefined) {
         return res.json({
           status: 401,
           message: 'Wrong email or password!'
         })
       } else {
-        return promisify.query('SELECT COUNT(`token`) AS count FROM `logged` WHERE `id_user` = ?', [userId[0].userId])
+        user_id = userId[0].id
+        return promisify.query('SELECT COUNT(`token`) AS count FROM `logged` WHERE `id_user` = ?', [user_id])
       }
     }).then(wasLoggedBefore => {
+      const timeStamp = moment().utc().format('YYYY-MM-DD HH:mm:ss')
+      const newToken = crypto.createHash('md5').update(timeStamp).digest('hex')
       if (wasLoggedBefore[0].count > 0) {
-        
+        db.query('UPDATE `logged` SET `token` = ?, `timestamp` = ? WHERE `id_user` = ?', [newToken, timeStamp, user_id], (error, results, fields) => {
+          res.json({
+            status: (error) ? 401 : 200,
+            message: (error) ? `Error! ${error.sqlMessage}` : null,
+            result: (error) ? `Token not setted!` : newToken
+          })
+        })
       } else {
-        
+        db.query('INSERT INTO `logged` SET `token` = ?, `timestamp` = ?, `id_user` = ?', [newToken, timeStamp, user_id], (error, results, fields) => {
+          res.json({
+            status: (error) ? 401 : 200,
+            message: (error) ? `Error! ${error.sqlMessage}` : null,
+            result: (error) ? `Token not setted!` : newToken
+          })
+        })
       }
     })
 })
