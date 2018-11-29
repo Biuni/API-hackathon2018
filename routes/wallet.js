010f0dcc737302a6eb1052716fa297e41af25721
Biuni <info.gianlucabonifazi@gmail.com>
2018-11-29 16:18:01 +0100

    Buy product API

--- routes/wallet.js
+++ routes/wallet.js
@@ -16,10 +16,10 @@ router.get('/transaction/:token', (req, res, next) => {
   const userToken = req.params.token
   let error = 1
   let msgerr = ''
-  promisify.query('SELECT id_user FROM logged WHERE `token` = ?', [userToken])
+  promisify.query('SELECT `id_user` FROM `logged` WHERE `token` = ?', [userToken])
     .then(userId => {
       if (typeof userId !== 'undefined' && userId.length > 0) {
-        return promisify.query('SELECT * FROM transaction WHERE `id_user` = ?', [userId[0].id_user])
+        return promisify.query('SELECT * FROM `transaction` WHERE `id_user` = ?', [userId[0].id_user])
       } else {
         error = 0
         msgerr = 'User not exist!'
@@ -49,4 +49,38 @@ router.get('/transaction/:token', (req, res, next) => {
     })
 })
 
+/**
+ * @api - {POST} - /wallet/buy - Buy an item
+ * @apiName - BuyItem
+ * @apiGroup - Wallet
+ *
+ * @apiParam - {String} id_product  - Product's id.
+ * @apiParam - {String} cost        - Product's cost.
+ * @apiParam - {String} user        - User token.
+ */
+router.post('/buy', (req, res, next) => {
+  if (req.body.id_product === '' || req.body.id_product === undefined ||
+    req.body.cost === '' || req.body.cost === undefined ||
+    req.body.user === '' || req.body.user === undefined) {
+    return res.json({
+      status: 0,
+      message: 'Error. Try again!'
+    })
+  }
+  promisify.query('SELECT `id_user` FROM `logged` WHERE `token` = ?', [req.body.user])
+    .then(userId => {
+      var newTransaction = {
+        id_user: userId[0].id_user,
+        credit: req.body.cost,
+        product_id: req.body.id_product
+      }
+      db.query('INSERT INTO `transaction` SET ?', newTransaction, (error, results, fields) => {
+        res.json({
+          status: (error) ? 0 : 1,
+          message: (error) ? `Error! ${error.sqlMessage}` : null,
+          result: (error) ? `Transaction not registered!` : 'Product buyed!'
+        })
+      })
+    })
+})
 module.exports = router
